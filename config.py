2bd1cb9d3a04196112f65102387c52e648e617e1
Alireza <sohrab.ark@gmail.com>
2018-06-16 11:51:08 +0200

    change parameters

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = dict()
 config["batch_size"] = 32
-config["total_steps"] = 400000
+config["total_steps"] = 200000
 config["validate_every"] = 4000
-config["validate_for"] = 1300
+config["validate_for"] = 1400
 config["save_every"] = 3 * config["validate_every"]
 
 config["n_filters"] =    [16, 32, 128, 128, 256, 256]
@@ -14,7 +14,7 @@ config["fc_layers"] = [256, 128]
 config["learning_rate"] = [0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001]
 config["decay_rate"] = 0.96
 # Usually decay every half of epochs
-config["decay_step"] = 50000
+config["decay_step"] = 40000
 config["optimizer"] = "adam"
 config["keep_prob"] = 0.75
 config["MAX_GRADIANT_NORM"] = 5.0
@@ -23,15 +23,16 @@ config["image_width"] = 192
 config["image_height"] = 192
 config["image_channel"] = 1
 # Output shape
-config["output_dim"] = 4
+config["output_dim"] = 3
+config["output_weights"] = [2.0, 2.0, 1.0, 1.0, 0.5]
 
 # Augmenation parameters
 config["prob_downscale"] = 0.75
 config["max_downscale"] = 0.5
 config["min_downscale"] = 0.9
 
-config["prob_reflection"] = 0.75
-config["min_reflection"] = 0.35
+config["prob_reflection"] = 0.5
+config["min_reflection"] = 0.25
 config["max_reflection"] = 0.75
 
 config["prob_blur"] = 0.5
@@ -40,9 +41,10 @@ config["max_blurSize"] = 9
 config["min_sigmaRatio"] = 0.25
 config["max_sigmaRatio"] = 0.75
 
-config["prob_occlusion"] = 0.5
-config["min_occlusion"] = 0.25
-config["max_occlusion"] = 0.50
+# config["prob_occlusion"] = 0.5
+config["min_occlusion"] = 0.05
+config["max_occlusion"] = 0.25
+config["occlusion_max_obj"] = 6
 
 
 # TODO:

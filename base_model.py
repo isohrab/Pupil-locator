b2d1e303d06d783b7fee5f016f8df81e361e3e12
Alireza <sohrab.ark@gmail.com>
2018-06-29 17:23:19 +0200

    add inception model

---
+++ base_model.py
@@ -0,0 +1,156 @@
+import tensorflow as tf
+
+
+# YOLO implementation
+# https://github.com/WojciechMormul/yolo2/blob/master/train.py
+class BaseModel(object):
+    """
+    Convolution model:
+    """
+
+    def __init__(self, model_name, cfg, logger):
+        self.cfg = cfg
+        self.model_name = model_name
+        self.logger = logger
+        self.l2beta = cfg["l2_beta"]
+        self.model_dir = "models/" + model_name + "/"
+        self.mode = 'train'
+        self.max_gradient_norm = cfg["MAX_GRADIANT_NORM"]
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
+        self.global_epoch_step = tf.Variable(0, trainable=False, name='global_epoch_step')
+        self.global_epoch_step_op = tf.assign(self.global_epoch_step, self.global_epoch_step + 1)
+
+        self.update = None
+        self.loss = None
+        self.logits = None
+
+    def init_placeholders(self):
+        # shape: [Batch_size, Width, Height, Channels]
+        self.X = tf.placeholder(dtype=tf.float32,
+                                shape=(None,
+                                       self.cfg["image_width"],
+                                       self.cfg["image_height"],
+                                       self.cfg["image_channel"]),
+                                name="ismages_input")
+
+        # shape: [Batch_size, 5] (x,y,w,h,a)
+        self.Y = tf.placeholder(dtype=tf.float32,
+                                shape=(None, self.cfg["output_dim"]),
+                                name="ground_truth")
+
+        self.keep_prob = tf.placeholder(dtype=tf.float32,
+                                        shape=(),
+                                        name="keep_prob")
+
+        self.train_flag = tf.placeholder(dtype=tf.bool, name='flag_placeholder')
+
+        self.learning_rate = tf.placeholder(dtype=tf.float32, shape=(), name="learning_rate")
+
+    def init_optimizer(self):
+        print("setting optimizer..")
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            trainable_params = tf.trainable_variables()
+
+            opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+
+            # add l2 loss
+            l2_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+            tf.summary.scalar("l2_loss", l2_loss)
+
+            final_loss = self.loss + l2_loss
+            # Training summary for the current batch_loss
+            tf.summary.scalar('final_loss', final_loss)
+
+            # Compute gradients of loss w.r.t. all trainable variables
+            gradients = tf.gradients(final_loss, trainable_params)
+
+            # Clip gradients by a given maximum_gradient_norm
+            clip_gradients, _ = tf.clip_by_global_norm(gradients, self.max_gradient_norm)
+
+            # Update the model
+            self.update = opt.apply_gradients(zip(clip_gradients, trainable_params),
+                                              global_step=self.global_step)
+
+    def train(self, sess, images, labels, keep_prob, lr):
+        """Run a train step of the model feeding the given inputs.
+        Args:
+        session: tensorflow session to use.
+        encoder_inputs: a numpy int matrix of [batch_size, max_source_time_steps]
+            to feed as encoder inputs
+        encoder_inputs_length: a numpy int vector of [batch_size]
+            to feed as sequence lengths for each element in the given batch
+        Returns:
+            A triple consisting of gradient norm (or None if we did not do backward),
+        average perplexity, and the outputs.
+        """
+        # Check if the model is 'training' mode
+        self.mode = 'train'
+
+        input_feed = {self.X.name: images,
+                      self.Y.name: labels,
+                      self.keep_prob.name: keep_prob,
+                      self.train_flag.name: True,
+                      self.learning_rate.name: lr}
+
+        output_feed = [self.update,  # Update Op that does optimization
+                       self.loss,  # Loss for current batch
+                       self.summary_op]
+
+        outputs = sess.run(output_feed, input_feed)
+        return outputs[1], outputs[2]
+
+    def eval(self, sess, images, labels):
+        """Run a evaluation step of the model feeding the given inputs.
+        Args:
+        session: tensorflow session to use.
+        encoder_inputs: a numpy int matrix of [batch_size, max_source_time_steps]
+        to feed as encoder inputs
+        encoder_inputs_length: a numpy int vector of [batch_size]
+        to feed as sequence lengths for each element in the given batch
+        Returns:
+        A triple consisting of gradient norm (or None if we did not do backward),
+        average perplexity, and the outputs.
+        """
+        self.mode = "eval"
+        input_feed = {self.X.name: images,
+                      self.Y.name: labels,
+                      self.keep_prob.name: 1.0,
+                      self.train_flag.name: False}
+
+        output_feed = [self.loss,  # Loss for current batch
+                       self.summary_op,
+                       self.logits]
+
+        outputs = sess.run(output_feed, input_feed)
+        return outputs[0], outputs[1], outputs[2]
+
+    def predict(self, sess, images):
+        self.mode = 'test'
+        # Input feeds for dropout
+        input_feed = {self.X.name: images,
+                      self.keep_prob.name: 1.0,
+                      self.train_flag.name: False}
+
+        output_feed = [self.logits]
+        outputs = sess.run(output_feed, input_feed)
+
+        return outputs[0]
+
+    def restore(self, sess, path, var_list=None):
+        # var_list = None returns the list of all saveable variables
+        saver = tf.train.Saver(var_list)
+        saver.restore(sess, save_path=path)
+        self.logger.log('model restored from %s' % path)
+
+    def variable_summaries(self, var):
+        """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+        with tf.name_scope('summaries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)

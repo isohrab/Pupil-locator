060a3bb730c313cbe74a21bfcfbdbd37d590be0d
Alireza <sohrab.ark@gmail.com>
2018-08-08 11:05:31 +0200

    Mild augmentation setting

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 config = dict()
-config["batch_size"] = 32
+config["batch_size"] = 64
 config["total_steps"] = 400000
-config["validate_every"] = 4000
-config["validate_for"] = 1400
+config["validate_every"] = 2000
+config["validate_for"] = 700
 config["save_every"] = 3 * config["validate_every"]
+config["n_epochs"] = 50
 
 config["n_filters"] =    [16, 32, 128, 128, 256, 256]
 config["filter_sizes"] = [3, 3  , 3  , 3  , 3  , 3]
@@ -11,11 +12,11 @@ config["max_pool"] =     [1, 1  , 1  , 1  , 1  , 1]
 # check layer size >>    [96,48, 24,12, 6 , 3 ]
 config["fc_layers"] = [256, 128]
 # check layer size >>    [96, 48, 24, 12 , ]
-config["learning_rate"] = [0.0009, 0.0006, 0.0003, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001]
+config["learning_rate"] = [0.001, 0.0009, 0.0006, 0.0003, 0.0001, 0.00005, 0.00001]
 config["decay_rate"] = 0.96
 # Usually decay every half of epochs
 config["decay_step"] = 40000
-config["optimizer"] = "adam"
+config["optimizer"] = "RMSProb"
 config["keep_prob"] = 0.75
 config["MAX_GRADIANT_NORM"] = 5.0
 # input info
@@ -47,18 +48,21 @@ config["max_occlusion"] = 0.25
 config["occlusion_max_obj"] = 6
 
 # exposure on noisy frames
-config["prob_exposure"] = 0.75
-config["min_exposure"] = 1.2
+config["prob_exposure"] = 0.5
+config["min_exposure"] = 0.8
 config["max_exposure"] = 1.8
 
 # crop input image
-config["crop_probability"] = 0.75
-config["crop_min_ratio"] = 0.5
-config["crop_max_ratio"] = 0.9
+config["crop_probability"] = 0.5
+config["crop_min_ratio"] = 0.8
+config["crop_max_ratio"] = 0.95
 
 # flip image
 config["flip_probability"] = 0.5
 
 # L2 regularization
-config["l2_beta"] = 0.01
+config["l2_beta"] = 0.00003
+
+# VOC configs
+config["voc_outputs"] = 21
 
